fa7bc10df7b8cfdf91bea0728a23ed104ad01e7b
ARFOGRAFEN <165585342+ARFOGRAFEN@users.noreply.github.com>
2025-11-25 10:45:37 +0300

    Update test_items.py

--- prev_module/test_items.py
+++ prev_module/test_items.py
@@ -1,1 +1,15 @@
+from selenium.webdriver.common.by import By
+import time
 
+def test_add_to_cart_button(browser):
+    # Открываем страницу товара
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    
+    # Для визуальной проверки языка
+    time.sleep(30)
+    
+    # Проверяем наличие кнопки добавления в корзину
+    add_to_cart_button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    
+    # Проверяем, что кнопка отображается на странице
+    assert add_to_cart_button.is_displayed(), "Add to cart button is not displayed"
